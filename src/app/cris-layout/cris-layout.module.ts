1743b39f345723e41c4f5cc2567118099edac8d5
Pratik Rajkotiya <pratik.r@peerbits.com>
2021-12-29 12:26:04 +0100

    [DSC-373] WIP

--- src/app/cris-layout/cris-layout.module.ts
+++ src/app/cris-layout/cris-layout.module.ts
@@ -46,6 +46,7 @@ import { MetadataRenderComponent } from './cris-layout-matrix/cris-layout-box-co
 import { MiradorViewerModule } from '../item-page/mirador-viewer/mirador-viewer.module';
 import { ComcolModule } from '../shared/comcol/comcol.module';
 import { SearchModule } from '../shared/search/search.module';
+import { CrisrefMoreComponent } from './cris-layout-matrix/cris-layout-box-container/crisref-more/crisref-more.component';
 
 const ENTRY_COMPONENTS = [
   // put only entry components that use custom decorator
@@ -94,7 +95,8 @@ const ENTRY_COMPONENTS = [
     DsDatePipe,
     RowComponent,
     MetadataContainerComponent,
-    MetadataRenderComponent
+    MetadataRenderComponent,
+    CrisrefMoreComponent
   ],
   imports: [
     CommonModule,
