a9c5247631290543b7f6172fb9863ff247d49d18
Davide Negretti <davide.negretti@4science.it>
2022-01-12 13:36:19 +0100

    [IIIF-71] Test fixed

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/iiif-toolbar/cris-layout-iiif-toolbar-box.component.spec.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/iiif-toolbar/cris-layout-iiif-toolbar-box.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CrisLayoutIIIFToolbarBoxComponent } from './cris-layout-iiif-toolbar-box.component';
+import { TranslateService } from '@ngx-translate/core';
+import { getMockTranslateService } from '../../../../../shared/mocks/translate.service.mock';
+import { ActivatedRoute, Router } from '@angular/router';
 
 describe('CrisLayoutIiifToolbarBoxComponent', () => {
   let component: CrisLayoutIIIFToolbarBoxComponent;
@@ -8,7 +11,14 @@ describe('CrisLayoutIiifToolbarBoxComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CrisLayoutIIIFToolbarBoxComponent ]
+      declarations: [ CrisLayoutIIIFToolbarBoxComponent ],
+      providers: [
+        { provide: 'boxProvider', useValue: {} },
+        { provide: 'itemProvider', useValue: {} },
+        { provide: TranslateService, useValue: getMockTranslateService() },
+        { provide: Router, useValue: {} },
+        { provide: ActivatedRoute, useValue: {} },
+      ]
     })
     .compileComponents();
   });
