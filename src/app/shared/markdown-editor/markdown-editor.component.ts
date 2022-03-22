ca871c56bfed4c61445d5d9adfa366c0ab70bb40
Sufiyan Shaikh <sufiyan.shaikh@peerbits.com>
2022-03-22 17:32:35 +0530

    [DSC-526] Improvements of user agreement - formatting

--- src/app/shared/markdown-editor/markdown-editor.component.ts
+++ src/app/shared/markdown-editor/markdown-editor.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, HostBinding,  Input,    OnInit, Output } from '@angular/core';
-import '@github/markdown-toolbar-element';
 
 @Component({
   selector: 'ds-markdown-editor',
@@ -9,48 +8,35 @@ import '@github/markdown-toolbar-element';
 export class MarkdownEditorComponent implements OnInit {
   // to allow multiple textarea on the same screen, need to set an uniqueId for the textarea
   controlId: string;
+  /**
+   * Markdown Editor String value
+   */
   @Input() editValue: string;
-  @HostBinding('class.focus') isFocus: boolean;
+  /**
+   * Markdown Editor String value Emitter
+   */
   @Output() editValueChange = new EventEmitter();
   /**
    * Nu markdown library options (default is chinese)
    */
-   options = {
+  options = {
     minHeight: 200,
     lang: 'en_US',
     mode: 'ir',
     preview: {
       markdown: {
         codeBlockPreview: false,
-      }
+      },
+      actions: [
+        'desktop', 'tablet', 'mobile'
+      ]
     },
     toolbar: [
-      'emoji',
-      'headings',
-      'bold',
-      'italic',
-      'strike',
-      'link',
-      '|',
-      'list',
-      'ordered-list',
-      'check',
-      'outdent',
-      'indent',
-      'table',
-      '|',
-      'quote',
-      'line',
-      'code',
-      'inline-code',
-      'insert-before',
-      'insert-after',
-      '|',
-      'undo',
-      'redo',
-      '|',
-      'fullscreen',
-      'preview'
+      'emoji', 'headings', 'bold', 'italic', 'strike', 'link', '|',
+      'list', 'ordered-list', 'check', 'outdent', 'indent', 'table', '|',
+      'quote', 'line', 'code', 'inline-code', 'insert-before', 'insert-after', '|',
+      'undo', 'redo', '|',
+      'fullscreen', 'preview'
     ],
   };
 
@@ -61,14 +47,9 @@ export class MarkdownEditorComponent implements OnInit {
     this.controlId = `MarkdownEditor-${Math.floor(100000 * Math.random())}`;
   }
 
-  focus() {
-    this.isFocus = true;
-  }
-
-  blur() {
-    this.isFocus = false;
-  }
-
+  /**
+   * Markdown Editor String value Emitter Function
+   */
   pushData() {
     console.log(this.editValue);
     this.editValueChange.emit(this.editValue);
