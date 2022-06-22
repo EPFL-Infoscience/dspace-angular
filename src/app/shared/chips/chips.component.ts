88e35ea18310c1ef4e9851dbed9cf90d25768281
Sufiyan Shaikh <sufian.shaikh@peerbits.com>
2022-06-22 19:03:39 +0530

    [UXP-11] Husky Commit

--- src/app/shared/chips/chips.component.ts
+++ src/app/shared/chips/chips.component.ts
@@ -9,7 +9,6 @@ import { UploaderService } from '../uploader/uploader.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Options } from 'sortablejs';
 import { BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
 const TOOLTIP_TEXT_LIMIT = 21;
 @Component({
   selector: 'ds-chips',
@@ -37,8 +36,7 @@ export class ChipsComponent implements OnChanges {
   constructor(
     private cdr: ChangeDetectorRef,
     private uploaderService: UploaderService,
-    private translate: TranslateService,
-    private router: Router) {
+    private translate: TranslateService) {
 
     this.options = {
       animation: 300,
