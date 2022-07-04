badb26010b05e99e104ed79f11ab1e06e793a641
Alisa Ismailati <alisa.ismailati@4science.com>
2022-07-04 18:20:59 +0200

    [DSC-477] (Partial commit) Merge items object structure and implementation

--- src/app/deduplication/deduplication-merge/deduplication-merge.component.ts
+++ src/app/deduplication/deduplication-merge/deduplication-merge.component.ts
@@ -1,31 +1,37 @@
+import { Bitstream } from './../../core/shared/bitstream.model';
 import { MetadataValue } from './../../core/shared/metadata.models';
 import { Item } from './../../core/shared/item.model';
 import { Observable } from 'rxjs/internal/Observable';
 import { DeduplicationStateService } from './../deduplication-state.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DeduplicationItemsService } from './deduplication-items.service';
-import { map } from 'rxjs/operators';
+import { map,  concatMap } from 'rxjs/operators';
 import { hasValue } from '../../shared/empty.util';
 import { ActivatedRoute } from '@angular/router';
+import { DsGetBundlePipe } from './ds-get-bundle.pipe';
 
 @Component({
   selector: 'ds-deduplication-merge',
   templateUrl: './deduplication-merge.component.html',
   styleUrls: ['./deduplication-merge.component.scss'],
+  providers: [DsGetBundlePipe],
 })
 export class DeduplicationMergeComponent implements OnInit, OnDestroy {
   protected storedItemIds$: Observable<string[]>;
 
+  protected setId: string;
+
   public itemsToCompare: ItemData[] = [];
 
-  public differencesArray: string[][] = [];
+  protected mergedMetadataFields: ItemsMetadataField[] = [];
 
-  public setId: string;
+  protected bitstreamList: string[] = [];
 
   constructor(
     private deduplicationStateService: DeduplicationStateService,
     private deduplicationItemsService: DeduplicationItemsService,
-    private route: ActivatedRoute,
+    private getBitstreamsPipe: DsGetBundlePipe,
+    private route: ActivatedRoute
   ) {
     this.storedItemIds$ = this.deduplicationStateService.getItemsToCompare();
     this.getItemsData();
@@ -33,7 +39,10 @@ export class DeduplicationMergeComponent implements OnInit, OnDestroy {
     this.setId = this.route.snapshot.params.setId;
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.buildMergeObject();
+    this.getItembitstreams();
+  }
 
   public getItemsData() {
     this.storedItemIds$.subscribe((itemIds: string[]) => {
@@ -61,9 +70,77 @@ export class DeduplicationMergeComponent implements OnInit, OnDestroy {
 
   merge() {
     // TODO: Implement logic
+    let mergedItems: MergeItems = {
+      setId: this.setId,
+      bitstreams: this.bitstreamList,
+      metadata: this.mergedMetadataFields,
+    };
+
+    console.log(mergedItems, 'mergedItems');
+
   }
 
+  onValueSelect(
+    field: string,
+    itemLink: string,
+    place: number,
+    selectType: 'single' | 'multiple'
+  ) {
+    let metadataSourceIdx = this.mergedMetadataFields
+      .find((x) => x.metadataField === field)
+      .sources.findIndex((x) => x.item === itemLink);
 
+    if (selectType === 'single' || metadataSourceIdx > -1) {
+      // if the item is in the list, remove it.
+      this.mergedMetadataFields
+        .find((x) => x.metadataField === field)
+        .sources.splice(metadataSourceIdx, 1);
+    }
+
+    // if the item is not in the list, add it
+    if (metadataSourceIdx < 0) {
+      this.mergedMetadataFields
+        .find((x) => x.metadataField === field)
+        .sources.push({
+          item: itemLink,
+          place: place,
+        });
+    }
+  }
+
+  private buildMergeObject() {
+    this.itemsToCompare[0]?.object$.subscribe((res: Item) => {
+      if (res) {
+        let keys = Object.keys(res.metadata);
+        this.mergedMetadataFields = keys.map((key) => {
+          return {
+            metadataField: key,
+            sources: [],
+          };
+        });
+      }
+    });
+  }
+
+  private getItembitstreams() {
+    this.itemsToCompare.map((item) => {
+      this.getBitstreamsPipe
+        .transform(item.object$)
+        .pipe(
+          concatMap((res$: Observable<Bitstream[]>) => {
+            return res$.pipe(
+              map((bitstreams: Bitstream[]) =>
+                 bitstreams
+              )
+            );
+          })
+        )
+        .subscribe((bitstreams: Bitstream[]) => {
+          let linksPerItem = bitstreams.map((b) => b._links.self.href);
+          this.bitstreamList = this.bitstreamList.concat(...linksPerItem);
+        });
+    });
+  }
 
   showDiff(keyvalue, asdf?) {
     for (let index = 0; index < this.itemsToCompare.length; index++) {
@@ -72,7 +149,6 @@ export class DeduplicationMergeComponent implements OnInit, OnDestroy {
       item.subscribe((res: Item) => {
         res.metadata[keyvalue.key].forEach((metadataValue: MetadataValue) => {
           console.log(metadataValue, 'metadataValue');
-
         });
       });
     }
@@ -96,4 +172,20 @@ export class DeduplicationMergeComponent implements OnInit, OnDestroy {
 export interface ItemData {
   object$: Observable<Item>;
   color: string;
+}
+
+export interface MergeItems {
+  setId: string;
+  bitstreams: string[];
+  metadata: ItemsMetadataField[];
+}
+
+export interface ItemsMetadataField {
+  metadataField: string;
+  sources: ItemMetadataSource[];
+}
+
+export interface ItemMetadataSource {
+  item: string;
+  place: number;
 }
