1305467483d97b5887012b7e7d0c6116ed588db9
Davide Negretti <davide.negretti@4science.it>
2022-08-12 14:22:25 +0200

    [CST-6664] Show additional metadata in search results

--- src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
+++ src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { listableObjectComponent } from '../../../../../object-collection/shared/listable-object/listable-object.decorator';
 import { ViewMode } from '../../../../../../core/shared/view-mode.model';
 import { ItemSearchResult } from '../../../../../object-collection/shared/item-search-result.model';
@@ -8,6 +8,8 @@ import { getItemPageRoute } from '../../../../../../item-page/item-page-routing-
 import { Context } from '../../../../../../core/shared/context.model';
 import { TruncatableService } from '../../../../../truncatable/truncatable.service';
 import { DSONameService } from '../../../../../../core/breadcrumbs/dso-name.service';
+import {hasValue, isNotEmpty} from '../../../../../empty.util';
+import { environment } from 'src/environments/environment';
 
 @listableObjectComponent('PublicationSearchResult', ViewMode.ListElement)
 @listableObjectComponent(ItemSearchResult, ViewMode.ListElement)
@@ -20,7 +22,7 @@ import { DSONameService } from '../../../../../../core/breadcrumbs/dso-name.serv
 /**
  * The component for displaying a list element for an item search result of the type Publication
  */
-export class ItemSearchResultListElementComponent extends SearchResultListElementComponent<ItemSearchResult, Item> {
+export class ItemSearchResultListElementComponent extends SearchResultListElementComponent<ItemSearchResult, Item> implements OnInit {
 
   @Input() hideMetrics = false;
 
@@ -33,8 +35,17 @@ export class ItemSearchResultListElementComponent extends SearchResultListElemen
    */
   itemPageRoute: string;
 
+  /**
+   * A list of additional metadata fields to display
+   */
+  public additionalMetadataFields: string[];
+
   ngOnInit(): void {
     super.ngOnInit();
     this.itemPageRoute = getItemPageRoute(this.dso);
+    if (hasValue(environment.myDSpace) && isNotEmpty(environment.myDSpace.additionalMetadataFields)) {
+      this.additionalMetadataFields = environment.myDSpace.additionalMetadataFields;
+    }
   }
+
 }
