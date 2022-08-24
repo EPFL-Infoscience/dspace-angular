0bba0e6f9a75b82149dfc809a83f90abc252077d
Davide Negretti <davide.negretti@4science.it>
2022-08-24 12:51:21 +0200

    [CST-6663] Upload language files - New uploader WIP

--- src/app/admin/admin-language-files/admin-language-files.component.ts
+++ src/app/admin/admin-language-files/admin-language-files.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { RequestService } from '../../core/data/request.service';
+import { map } from 'rxjs/operators';
+import { HALEndpointService } from '../../core/shared/hal-endpoint.service';
+import { UploaderOptions } from '../../shared/uploader/uploader-options.model';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'ds-admin-language-files',
   templateUrl: './admin-language-files.component.html',
   styleUrls: ['./admin-language-files.component.scss']
 })
-export class AdminLanguageFilesComponent {
+export class AdminLanguageFilesComponent implements OnInit {
 
   fileObj = new Map<string, File>();
 
@@ -18,32 +22,36 @@ export class AdminLanguageFilesComponent {
 
   labelPrefix = 'admin.language-labels';
 
-  constructor(
-      protected notificationsService: NotificationsService,
-      protected translate: TranslateService,
-      protected requestService: RequestService,
-  ) { }
+  uploadFilesOptions$: Observable<UploaderOptions>;
 
+  constructor(
+    protected notificationsService: NotificationsService,
+    protected translate: TranslateService,
+    protected requestService: RequestService,
+    private halService: HALEndpointService,
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit() {
+
+    this.uploadFilesOptions$ = this.halService.getEndpoint('adminfile').pipe(
+      map((endpointURL) => Object.assign(new UploaderOptions(), {
+        url: endpointURL,
+        authToken: this.authService.buildAuthHeader(),
+        maxFileNumber: 1,
+      }))
+    );
+
+  }
+
+  doSomething($event?: any) {
+    this.notificationsService.info('TEST', 'test');
+    console.log($event);
+  }
 
   setFile($event: File, langCode: string) {
     this.fileObj.set(langCode, $event);
-  }
-
-  uploadFiles() {
-    if (this.fileObj.size) {
-
-      this.fileObj.forEach((file, langCode) => {
-
-        const postHref = '';
-
-        const requestId = this.requestService.generateRequestId();
-
-
-      });
-
-    } else {
-      this.notificationsService.warning(this.getTranslation('notification.no-files.title'), this.getTranslation('notification.no-files.content'));
-    }
   }
 
   getLabel(value: string): string {
@@ -53,4 +61,5 @@ export class AdminLanguageFilesComponent {
   getTranslation(value: string): Observable<string> {
     return this.translate.get(`${this.labelPrefix}.${value}`);
   }
+
 }
