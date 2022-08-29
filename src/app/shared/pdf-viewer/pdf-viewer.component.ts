b3f014a64c032f73c5dd8cdb39b7320f9c6fc3f5
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-08-29 18:58:56 +0200

    fix pdfjs viewer

--- src/app/shared/pdf-viewer/pdf-viewer.component.ts
+++ src/app/shared/pdf-viewer/pdf-viewer.component.ts
@@ -1,6 +1,21 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+
 import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
-import { isUndefined } from '../empty.util';
+
+import { hasValue, isEmpty, isNotEmpty } from '../empty.util';
+import { debounceTime, filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ds-pdf-viewer',
@@ -8,35 +23,65 @@ import { isUndefined } from '../empty.util';
   styleUrls: ['./pdf-viewer.component.scss']
 })
 /**
-  * Component that displays the pdf content using pdfjs.
-  */
-export class PdfViewerComponent implements OnChanges {
+ * Component that displays the pdf content using pdfjs.
+ */
+export class PdfViewerComponent implements OnChanges, OnDestroy {
 
   /**
-    * The pdf link to be displayed in the preview pdf section
-    */
+   * The pdf link to be displayed in the preview pdf section
+   */
   @Input() pdfSrc: string;
 
+  private sub: Subscription;
   /**
-    * Reference of the accordion
-    */
+   * Reference of the accordion
+   */
   @ViewChild('accordionRef') accordionRef: NgbAccordion;
 
   /**
-  * Reference of the pdf viewer
-  */
+   * Reference of the pdf viewer
+   */
   @ViewChild('pdfViewer') pdfViewer;
+  @ViewChildren('pdfViewer', { read: ElementRef }) pdfViewerList: QueryList<ElementRef>;
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit() {
+    if (isEmpty(this.pdfSrc)) {
+      this.accordionRef.collapseAll();
+    }
+
+    this.sub = this.pdfViewerList.changes.pipe(
+      filter(() => this.pdfViewerList?.length > 0 && this.pdfViewer),
+      debounceTime(1000)
+    ).subscribe(data => {
+        this.refreshPdfViewer(this.pdfSrc);
+      }
+    );
+  }
 
   /**
    * When changes happen to the pdf link refresh the pdf viewer and open accordion.
    */
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.hasOwnProperty('pdfSrc') && changes.pdfSrc.currentValue !== changes.pdfSrc.previousValue) {
+    if (isNotEmpty(changes?.pdfSrc?.currentValue)) {
       this.accordionRef.expand('pdf-viewer');
-      if (!isUndefined(changes.pdfSrc.previousValue)) {
-        this.pdfViewer.refresh();
-      }
+      this.refreshPdfViewer(changes.pdfSrc.currentValue);
     }
   }
 
+  private refreshPdfViewer(src: string) {
+    if (this.pdfViewer && isNotEmpty(src)) {
+      this.pdfViewer.pdfSrc = src;
+      this.pdfViewer.refresh();
+      this.cdr.detectChanges();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (hasValue(this.sub)) {
+      this.sub.unsubscribe();
+    }
+  }
 }
