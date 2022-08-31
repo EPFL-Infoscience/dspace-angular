8a9b4a29baed6c8981cbfec49b318379a1e1dede
Davide Negretti <davide.negretti@4science.it>
2022-08-31 16:27:55 +0200

    [CST-6640] Send email form

--- src/app/shared/mydspace-actions/claimed-task/email/claimed-task-actions-send-email.component.ts
+++ src/app/shared/mydspace-actions/claimed-task/email/claimed-task-actions-send-email.component.ts
@@ -3,21 +3,24 @@ import { rendersWorkflowTaskOption } from '../switcher/claimed-task-actions-deco
 import { NgbModalOptions } from '@ng-bootstrap/ng-bootstrap/modal/modal-config';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NotificationsService } from '../../../notifications/notifications.service';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { ClaimedTaskActionsAbstractComponent } from '../abstract/claimed-task-actions-abstract.component';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { SearchService } from '../../../../core/shared/search/search.service';
 import { RequestService } from '../../../../core/data/request.service';
-import {
-  ClaimedTaskActionsReturnToPoolComponent
-} from '../return-to-pool/claimed-task-actions-return-to-pool.component';
 import { getFirstSucceededRemoteDataPayload } from '../../../../core/shared/operators';
-import { map, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { WorkflowItem } from '../../../../core/submission/models/workflowitem.model';
 import { WorkflowItemDataService } from '../../../../core/submission/workflowitem-data.service';
 
 export const WORKFLOW_TASK_OPTION_EMAIL = 'email';
+
+interface Email {
+  templateName: string;
+  subject: string;
+  body: string;
+}
 
 @rendersWorkflowTaskOption(WORKFLOW_TASK_OPTION_EMAIL)
 @Component({
@@ -30,6 +33,7 @@ export class ClaimedTaskActionsSendEmailComponent extends ClaimedTaskActionsAbst
 
   option = WORKFLOW_TASK_OPTION_EMAIL;
 
+  email = <Email>{};
 
   templateList$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
@@ -46,11 +50,8 @@ export class ClaimedTaskActionsSendEmailComponent extends ClaimedTaskActionsAbst
     super(injector, router, notificationsService, translate, searchService, requestService);
   }
 
-  ngOnInit(): void {
-  }
-
-
   openEmailModal(template: TemplateRef<any>) {
+
 
     // TODO initialize template list
     this.templateList$.next(['TEMPLATE_1', 'TEMPLATE_2']);
@@ -59,7 +60,7 @@ export class ClaimedTaskActionsSendEmailComponent extends ClaimedTaskActionsAbst
       getFirstSucceededRemoteDataPayload(),
       map((workflowItem: WorkflowItem) => workflowItem._links.submitter.href),
       tap(console.log)
-    );
+    ).subscribe();
 
     /*const submitter$ = submitterHref$.pipe(
       mergeMap((res) => this.workflowItemService.findByHref(res)),
@@ -70,7 +71,18 @@ export class ClaimedTaskActionsSendEmailComponent extends ClaimedTaskActionsAbst
 
     const options: NgbModalOptions = {size: 'lg'};
     const modal = this.modalService.open(template, options);
-    modal.result.then(console.log).catch((err) => undefined);
+    modal.result.then(console.log).then(() => {
+      console.log('SEND EMAIL ' + JSON.stringify(this.email));
+    }).catch((err) => undefined);
+  }
+
+  onTemplateChosen() {
+    console.log(this.email.templateName);
+    of(undefined).subscribe((res) => {
+      this.email.subject = 'TEST SUBJECT';
+      this.email.body = 'lorem ipsum\ndolor sit amet';
+    });
+
   }
 
 }
