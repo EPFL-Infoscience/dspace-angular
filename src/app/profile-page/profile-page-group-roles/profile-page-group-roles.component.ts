7c4a8b82d9c0b31c8acccc7fe1e4f44abf546e38
Enea Jahollari <enea.jahollari@4science.com>
2023-03-31 14:54:37 +0200

    CST-8651 Initial implementation of group roles component

---
+++ src/app/profile-page/profile-page-group-roles/profile-page-group-roles.component.ts
@@ -0,0 +1,56 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { AuthService } from '../../core/auth/auth.service';
+import { EPerson } from '../../core/eperson/models/eperson.model';
+import { hasValue } from '../../shared/empty.util';
+import { followLink } from '../../shared/utils/follow-link-config.model';
+import { getFirstCompletedRemoteData, getRemoteDataPayload } from '../../core/shared/operators';
+import { EPersonDataService } from '../../core/eperson/eperson-data.service';
+
+@Component({
+  selector: 'ds-profile-page-group-roles',
+  templateUrl: './profile-page-group-roles.component.html',
+  styleUrls: [ './profile-page-group-roles.component.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProfilePageGroupRolesComponent {
+
+  private groupRoleId$ = this.route.params.pipe(map((p) => p.id as string));
+
+  groupRole$ = this.groupRoleId$.pipe(
+    switchMap((id: string) => this.userGroups$.pipe(
+      map((groups) => groups.find((group) => group.id === id))
+    ))
+  );
+
+  private userGroups$ = this.authService.getAuthenticatedUserFromStore().pipe(
+    filter((user: EPerson) => hasValue(user.id)),
+    switchMap((user: EPerson) => this.epersonService.findById(user.id, true, true, followLink('groups'))),
+    getFirstCompletedRemoteData(),
+    getRemoteDataPayload(),
+    switchMap((user: EPerson) => user.groups.pipe(
+      map((groups) => groups.payload.page)
+    ))
+  );
+
+  roles = [
+    {
+      name: 'browser repository structures',
+      anonymous: 'Y',
+      submitter: 'Y',
+    },
+    {
+      name: 'download reserved files',
+      anonymous: 'N',
+      submitter: 'N',
+    }
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private epersonService: EPersonDataService
+  ) {}
+
+}
--- src/app/profile-page/profile-page-routing.module.ts
+++ src/app/profile-page/profile-page-routing.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
 import { ThemedProfilePageComponent } from './themed-profile-page.component';
+import { ProfilePageGroupRolesComponent } from './profile-page-group-roles/profile-page-group-roles.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', pathMatch: 'full', component: ThemedProfilePageComponent, resolve: { breadcrumb: I18nBreadcrumbResolver }, data: { breadcrumbKey: 'profile', title: 'profile.title' } }
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ThemedProfilePageComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { breadcrumbKey: 'profile', title: 'profile.title' }
+      },
+      {
+        path: 'group-roles/:id',
+        component: ProfilePageGroupRolesComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { breadcrumbKey: 'profile.group-roles', title: 'profile.group-roles.title' }
+      }
     ])
   ]
 })
-export class ProfilePageRoutingModule {
-
-}
+export class ProfilePageRoutingModule {}
