3bc3d90c3ee1e28f327830d08d0137e05157ab5c
Andrea Barbasso <andrea.barbasso@4science.com>
2023-05-03 09:54:59 +0200

    [DSC-163] test fixed

--- src/app/item-page/viewer-provider/viewer-provider.component.spec.ts
+++ src/app/item-page/viewer-provider/viewer-provider.component.spec.ts
@@ -10,20 +10,25 @@ import { Location } from '@angular/common';
 import { AuthService } from '../../core/auth/auth.service';
 import { AuthServiceMock } from '../../shared/mocks/auth.service.mock';
 import { TranslateModule } from '@ngx-translate/core';
+import { SpyLocation } from '@angular/common/testing';
 
 describe('ViewerProviderComponent', () => {
   let component: ViewerProviderComponent;
   let fixture: ComponentFixture<ViewerProviderComponent>;
 
+  beforeAll(() => {
+    window.onbeforeunload = () => '';
+  });
+
   beforeEach(async () => {
-    location = jasmine.createSpyObj('location', ['back']);
+    // location = jasmine.createSpyObj('location', ['back', 'path']);
     await TestBed.configureTestingModule({
       declarations: [ ViewerProviderComponent ],
       imports: [ RouterTestingModule.withRoutes([]), TranslateModule.forRoot() ],
       providers: [
         {provide: Store, useValue: StoreMock},
         {provide: ItemDataService, useValue: {}},
-        {provide: Location, useValue: location},
+        {provide: Location, useValue: new SpyLocation()},
         {provide: AuthService, useValue: new AuthServiceMock()},
       ],
       schemas: [NO_ERRORS_SCHEMA]
