d2efb2f008b4534f51d645ca408e7117fd423019
Nikita Krivonosov <nikita.krivonosov@4science.com>
2023-05-22 09:24:44 +0200

    [DSC-890] Integration with Unpaywall API

--- src/app/submission/sections/unpaywall/submission-section-unpaywall.component.ts
+++ src/app/submission/sections/unpaywall/submission-section-unpaywall.component.ts
@@ -1,19 +1,15 @@
 import { Component, Inject, OnDestroy } from '@angular/core';
 import { renderSectionFor } from '../sections-decorator';
 import { SectionsType } from '../sections-type';
-import { WorkspaceitemDataService } from '../../../core/submission/workspaceitem-data.service';
-import { ActivatedRoute } from '@angular/router';
 import { SectionModelComponent } from '../models/section.model';
 import { SectionDataObject } from '../models/section-data.model';
 import { SectionsService } from '../sections.service';
-import { BehaviorSubject, interval, Observable, Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, forkJoin, interval, mergeMap, Observable, of, Subject, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { SubmissionState } from '../../submission.reducers';
 import { distinctUntilChanged, filter, map, switchMap, takeUntil } from 'rxjs/operators';
-import { submissionObjectFromIdSelector } from '../../selectors';
 import { SubmissionObjectEntry } from '../../objects/submission-objects.reducer';
-import { followLink } from '../../../shared/utils/follow-link-config.model';
-import { DisableSectionAction, EnableSectionAction } from '../../objects/submission-objects.actions';
+import { UpdateSectionVisibilityAction } from '../../objects/submission-objects.actions';
 import {
   WorkspaceitemSectionUnpaywallObject
 } from '../../../core/submission/models/workspaceitem-section-unpaywall-object';
@@ -30,12 +26,25 @@ import { hasValue, isEmpty, isNotEmpty } from '../../../shared/empty.util';
 import { NotificationsService } from '../../../shared/notifications/notifications.service';
 import parseSectionErrors from '../../utils/parseSectionErrors';
 import { TranslateService } from '@ngx-translate/core';
+import { JsonPatchOperationPathCombiner } from '../../../core/json-patch/builder/json-patch-operation-path-combiner';
+import {
+  SubmissionVisibilityType,
+  SubmissionVisibilityValue
+} from '../../../core/config/models/config-submission-section.model';
+import { SubmissionScopeType } from '../../../core/submission/submission-scope-type';
+import { submissionObjectFromIdSelector } from '../../selectors';
+import { DspaceRestService } from '../../../core/dspace-rest/dspace-rest.service';
+import { RestRequestMethod } from '../../../core/data/rest-request-method';
+import { Operation } from 'fast-json-patch';
+import { ResourceService } from '../../../core/services/resource.service';
+import { UnpaywallApi } from './models/unpaywall-api';
 
 const DOI_METADATA = 'dc.identifier.doi';
 const API_CHECK_INTERVAL = 3000;
-const FILE_NAME = 'unpaywall.json';
-const FILE_TYPE = 'application/json';
 
+/**
+ * This component represents a section that contains the submission unpaywall integration.
+ */
 @Component({
   selector: 'ds-submission-section-unpaywall-component',
   templateUrl: './submission-section-unpaywall.component.html',
@@ -45,23 +54,24 @@ const FILE_TYPE = 'application/json';
 export class SubmissionSectionUnpaywallComponent extends SectionModelComponent implements OnDestroy {
 
   public readonly UnpaywallSectionStatus = UnpaywallSectionStatus;
-  public status$ = new BehaviorSubject<string>(null);
-  public loading$ = new BehaviorSubject<boolean>(true);
+  public readonly status$ = new BehaviorSubject<UnpaywallSectionStatus>(null);
+  public readonly loading$ = new BehaviorSubject<boolean>(true);
+  public readonly section$ = new BehaviorSubject<WorkspaceitemSectionUnpaywallObject>(null);
+  private readonly unsubscribe$ = new Subject<void>();
   private checkIntervalSubscription: Subscription;
-  private section: WorkspaceitemSectionUnpaywallObject;
-  private unsubscribe$ = new Subject<void>();
+  readonly uploader = new FileUploader({ autoUpload: false });
 
   constructor(
     protected sectionService: SectionsService,
-    protected store: Store<SubmissionState>,
-    private workspaceItemService: WorkspaceitemDataService,
-    private route: ActivatedRoute,
+    private store: Store<SubmissionState>,
     private halService: HALEndpointService,
     private authService: AuthService,
     private submissionService: SubmissionService,
     private tokenExtractor: HttpXsrfTokenExtractor,
     private notificationsService: NotificationsService,
     private translate: TranslateService,
+    private resourceService: ResourceService,
+    private restApi: DspaceRestService,
     @Inject('collectionIdProvider') public injectedCollectionId: string,
     @Inject('sectionDataProvider') public injectedSectionData: SectionDataObject,
     @Inject('submissionIdProvider') public injectedSubmissionId: string
@@ -73,7 +83,7 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
     );
   }
 
-  public ngOnInit(): void {
+  protected onSectionInit(): void {
     this.store.select(submissionObjectFromIdSelector(this.submissionId))
       .pipe(
         takeUntil(this.unsubscribe$),
@@ -83,23 +93,43 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
       .subscribe(doiValue => !!doiValue ? this.showCurrentSection() : this.hideCurrentSection());
   }
 
-  public ngOnDestroy(): void {
+  protected onSectionDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
-    super.ngOnDestroy();
   }
 
-  public manualApiCheck(): void {
-    this.loading$.next(true);
-    this.checkApi().pipe(takeUntil(this.unsubscribe$)).subscribe(unpaywallSection => {
-        this.section = unpaywallSection;
-        this.status$.next(unpaywallSection?.status);
-        this.loading$.next(false);
+  public refreshApiCheck(): void {
+    this.startApiCheck(true);
+  }
+
+  public uploadFileIfNeeded(): void {
+    if (this.section$.getValue()?.status === UnpaywallSectionStatus.SUCCESSFUL) {
+      this.loading$.next(true);
+      const fileUrl = this.getFileUrl();
+      if (fileUrl) {
+        const fileName = fileUrl.split('/').pop();
+        forkJoin([
+          this.resourceService.download(fileUrl),
+          this.halService.getEndpoint(this.submissionService.getSubmissionObjectLinkName())
+        ]).pipe(
+          takeUntil(this.unsubscribe$),
+          map(([fileBlob, endpoint]) => {
+            const file = new File([fileBlob], fileName);
+            this.uploader.setOptions({
+              url: endpoint.concat(`/${this.submissionId}`),
+              authToken: this.authService.buildAuthHeader(),
+              autoUpload: false,
+              headers: [{ name: XSRF_REQUEST_HEADER, value: this.tokenExtractor.getToken() }]
+            });
+            return file;
+          }),
+          mergeMap((file) => this.uploadFile(file))
+        ).subscribe(() => this.loading$.next(false));
       }
-    );
+    }
   }
 
-  private startApiCheck(): void {
+  private startApiCheck(refreshRequired = false): void {
     this.loading$.next(true);
     if (this.checkIntervalSubscription && !this.checkIntervalSubscription.closed) {
       this.checkIntervalSubscription.unsubscribe();
@@ -107,16 +137,19 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
     this.checkIntervalSubscription = interval(API_CHECK_INTERVAL)
       .pipe(
         takeUntil(this.unsubscribe$),
-        switchMap(() => this.checkApi())
+        switchMap(() => this.checkApi(refreshRequired)),
+        filter(hasValue)
       )
-      .subscribe(unpaywallSection => {
-          if (!!unpaywallSection?.status && unpaywallSection?.status !== UnpaywallSectionStatus.PENDING) {
-            this.status$.next(unpaywallSection?.status);
-            this.section = unpaywallSection;
-            this.stopApiCheck();
-          }
+      .subscribe(section => {
+        this.section$.next(section);
+        this.status$.next(section.status);
+
+        const isLoading = !section?.status || section?.status === UnpaywallSectionStatus.PENDING;
+        this.loading$.next(isLoading);
+        if (!isLoading) {
+          this.stopApiCheck();
         }
-      );
+      });
   }
 
   private stopApiCheck(): void {
@@ -126,58 +159,52 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
     }
   }
 
-  private checkApi(): Observable<WorkspaceitemSectionUnpaywallObject> {
-    const id = this.route.snapshot.paramMap.get('id');
-    return this.workspaceItemService
-      .findById(id, false, false, followLink('item'))
+  private checkApi(refreshRequired = false): Observable<WorkspaceitemSectionUnpaywallObject> {
+    const pathCombiner = new JsonPatchOperationPathCombiner('sections', this.sectionData.id);
+    const path = pathCombiner.getPath('refresh');
+    const operation = { op: 'add', path: path.path, value: refreshRequired } as Operation;
+    const linkPath = this.submissionService.getSubmissionObjectLinkName();
+    return this.halService.getEndpoint(linkPath)
       .pipe(
-        map(value =>
-          (value.payload?.sections?.unpaywall as WorkspaceitemSectionUnpaywallObject))
+        takeUntil(this.unsubscribe$),
+        map((endpoint: string) => endpoint.concat(`/${this.submissionId}`)),
+        mergeMap((endpoint: string) => this.restApi.request(RestRequestMethod.PATCH, endpoint, [operation])),
+        map(response => response.payload?.sections?.unpaywall as WorkspaceitemSectionUnpaywallObject)
       );
   }
 
   private getDoiMetadataValue(value: SubmissionObjectEntry): string {
-    return value.sections[value.definition]?.data[DOI_METADATA];
+    return value.sections[value.definition]?.data?.[DOI_METADATA]?.[0]?.value;
   }
 
   private hideCurrentSection(): void {
     this.stopApiCheck();
-    this.store.dispatch(new DisableSectionAction(this.submissionId, this.injectedSectionData.id));
+    this.store.dispatch(new UpdateSectionVisibilityAction(this.submissionId, this.injectedSectionData.id, {
+      [SubmissionScopeType.WorkspaceItem]: SubmissionVisibilityValue.Hidden,
+      [SubmissionScopeType.EditItem]: SubmissionVisibilityValue.Hidden,
+      [SubmissionScopeType.WorkflowItem]: SubmissionVisibilityValue.Hidden
+    } as SubmissionVisibilityType));
   }
 
   private showCurrentSection(): void {
     this.startApiCheck();
-    this.store.dispatch(new EnableSectionAction(this.submissionId, this.injectedSectionData.id));
+    this.store.dispatch(new UpdateSectionVisibilityAction(this.submissionId, this.injectedSectionData.id, null));
   }
 
-  public uploadFileIfNeeded(): void {
-    if (this.section.status === UnpaywallSectionStatus.SUCCESSFUL) {
-      const file = new File([this.section.jsonRecord], FILE_NAME, {type: FILE_TYPE});
-      this.halService.getEndpoint(this.submissionService.getSubmissionObjectLinkName())
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe(endpointUrl => {
-          const uploader = new FileUploader({
-            url: endpointUrl.concat(`/${this.submissionId}`),
-            authToken: this.authService.buildAuthHeader(),
-            autoUpload: false,
-            headers: [{name: XSRF_REQUEST_HEADER, value: this.tokenExtractor.getToken()}]
-          });
-          this.uploadFile(uploader, file);
-        });
-    }
+  private getFileUrl(): string {
+    return (JSON.parse(this.section$.getValue()?.jsonRecord) as UnpaywallApi)?.best_oa_location?.url;
   }
 
-  private uploadFile(uploader: FileUploader, file: File): void {
+  private uploadFile(file: File): Observable<void> {
     const onSuccessItem$ = new BehaviorSubject<WorkspaceItem>(null);
-    onSuccessItem$
-      .pipe(
-        filter(hasValue),
-        takeUntil(this.unsubscribe$)
-      )
-      .subscribe(workspaceItem => this.updateUploadSection(workspaceItem));
-    uploader.onSuccessItem = (item, response) => onSuccessItem$.next(JSON.parse(response) as WorkspaceItem);
-    uploader.addToQueue([file]);
-    uploader.uploadAll();
+    onSuccessItem$.pipe(
+      filter(hasValue),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(workspaceItem => this.updateUploadSection(workspaceItem));
+    this.uploader.onSuccessItem = (item, response) => onSuccessItem$.next(JSON.parse(response) as WorkspaceItem);
+    this.uploader.addToQueue([file]);
+    this.uploader.uploadAll();
+    return onSuccessItem$.asObservable().pipe(filter(hasValue));
   }
 
   private updateUploadSection(workspaceItem: WorkspaceItem): void {
@@ -192,6 +219,7 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
           .pipe(takeUntil(this.unsubscribe$))
           .subscribe((isUpload) => {
             if (isUpload) {
+              this.sectionService.updateSectionData(this.submissionId, sectionId, sectionData, sectionErrors, sectionErrors);
               if ((isEmpty(sectionErrors))) {
                 this.notificationsService
                   .success(null, this.translate.get('submission.sections.upload.upload-successful'));
@@ -200,18 +228,13 @@ export class SubmissionSectionUnpaywallComponent extends SectionModelComponent i
               }
             }
           });
-        this.sectionService.updateSectionData(this.submissionId, sectionId, sectionData, sectionErrors, sectionErrors);
       });
     }
+    this.uploader.clearQueue();
   }
 
   protected getSectionStatus(): Observable<boolean> {
-    return undefined;
+    return of(true);
   }
 
-  protected onSectionDestroy(): void {
-  }
-
-  protected onSectionInit(): void {
-  }
 }
