f0e21c9f1461a1a9a2c665f90cdd03b9a8a3aff0
Nikita Krivonosov <nikita.krivonosov@4science.com>
2023-07-04 14:09:49 +0200

    [UXP-65] Create an unpaywall widget (metric)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -235,6 +235,7 @@ import {
 } from './metadata/schema-json-ld/schema-types/product/product-creative-work-schema-type';
 import { ProductDatasetSchemaType } from './metadata/schema-json-ld/schema-types/product/product-dataset-schema-type';
 import { PersonSchemaType } from './metadata/schema-json-ld/schema-types/Person/person-schema-type';
+import { UnpaywallItemService } from "./data/unpaywall-item.service";
 
 /**
  * When not in production, endpoint responses can be mocked for testing purposes
@@ -271,6 +272,7 @@ const PROVIDERS = [
   HALEndpointService,
   HostWindowService,
   ItemDataService,
+  UnpaywallItemService,
   MetadataService,
   SchemaJsonLDService,
   ObjectCacheService,
