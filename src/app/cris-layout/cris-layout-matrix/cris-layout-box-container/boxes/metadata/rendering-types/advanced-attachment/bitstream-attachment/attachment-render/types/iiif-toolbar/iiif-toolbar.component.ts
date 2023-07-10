fd788b3c2ef413dfa98bfca80e1726149be05f3e
Davide Negretti <davide.negretti@4science.com>
2023-07-10 16:22:05 +0200

    [UXP-72] Authorization check fixed

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/advanced-attachment/bitstream-attachment/attachment-render/types/iiif-toolbar/iiif-toolbar.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/advanced-attachment/bitstream-attachment/attachment-render/types/iiif-toolbar/iiif-toolbar.component.ts
@@ -13,6 +13,7 @@ import { FeatureID } from '../../../../../../../../../../../core/data/feature-au
 import { isNotEmpty } from '../../../../../../../../../../../shared/empty.util';
 import { AuthorizationDataService } from '../../../../../../../../../../../core/data/feature-authorization/authorization-data.service';
 import { of } from 'rxjs';
+import { Bitstream } from '../../../../../../../../../../../core/shared/bitstream.model';
 
 @Component({
   selector: 'ds-iiif-toolbar',
@@ -25,6 +26,9 @@ export class IIIFToolbarComponent implements OnInit {
   @Input()
   item: Item;
 
+  @Input()
+  bitstream: Bitstream;
+
   // The path to the REST manifest endpoint.
   manifestUrl: string;
 
@@ -33,7 +37,7 @@ export class IIIFToolbarComponent implements OnInit {
   isAuthorized$ = of(false);
 
   getObjectUrl() {
-    return isNotEmpty(this.item) ? this.item.self : undefined;
+    return isNotEmpty(this.bitstream) ? this.bitstream.self : undefined;
   }
 
   constructor(
