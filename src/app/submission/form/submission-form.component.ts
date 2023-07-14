126d069c93204f4011cf48e42fa637a43a14c74f
corrado lombardi <corrado.lombardi@4science.com>
2023-07-14 12:35:37 +0200

    fix on collection visibility

--- src/app/submission/form/submission-form.component.ts
+++ src/app/submission/form/submission-form.component.ts
@@ -227,7 +227,7 @@ export class SubmissionFormComponent implements OnChanges, OnDestroy {
         (section) => isEqual(section.sectionType, SectionsType.Collection)
       );
 
-   return isNotUndefined(submissionSectionModel.visibility) ? submissionSectionModel.visibility : null;
+   return isNotUndefined(submissionSectionModel) && isNotUndefined(submissionSectionModel.visibility) ? submissionSectionModel.visibility : null;
   }
 
   /**
