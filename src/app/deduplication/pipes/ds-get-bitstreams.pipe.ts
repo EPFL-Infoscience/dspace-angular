b097bbe32cecc80fed0c4757897d655441c3ae9a
yevhenii.lohatskyi <lohatskyi.yevhenii@4science.com>
2023-11-21 11:17:56 +0200

    [CST-10311] Refactor ds-get-bitstreams.pipe to get rid of nested observables structure

--- src/app/deduplication/pipes/ds-get-bitstreams.pipe.ts
+++ src/app/deduplication/pipes/ds-get-bitstreams.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable, of, switchMap } from 'rxjs';
-import { DSONameService } from './../../core/breadcrumbs/dso-name.service';
-import { PaginatedList } from './../../core/data/paginated-list.model';
-import { Bitstream } from './../../core/shared/bitstream.model';
-import { Bundle } from './../../core/shared/bundle.model';
-import { Item } from './../../core/shared/item.model';
-import { getAllSucceededRemoteDataPayload, getPaginatedListPayload } from './../../core/shared/operators';
+import { defaultIfEmpty, map } from 'rxjs/operators';
+import { combineLatest, Observable, of, switchMap } from 'rxjs';
+import { DSONameService } from '../../core/breadcrumbs/dso-name.service';
+import { PaginatedList } from '../../core/data/paginated-list.model';
+import { Bitstream } from '../../core/shared/bitstream.model';
+import { Bundle } from '../../core/shared/bundle.model';
+import { Item } from '../../core/shared/item.model';
+import { getAllSucceededRemoteDataPayload, getPaginatedListPayload } from '../../core/shared/operators';
 
 @Pipe({
   name: 'dsGetBitstreams',
@@ -19,27 +19,31 @@ export class GetBitstreamsPipe implements PipeTransform {
    * for the 'ORIGINAL' bundle
    * @param item Item object
    */
-  transform(item: Item): Observable<Observable<Bitstream[]>> {
+  transform(item: Item): Observable<Bitstream[]> {
     if (item && item.bundles) {
       return item.bundles.pipe(
         getAllSucceededRemoteDataPayload(),
         getPaginatedListPayload(),
         switchMap((bundle: Bundle[]) => {
-          if (bundle.length > 0) {
-            return bundle.map((b: Bundle) => {
+          const originalBundle = bundle.filter(b => this.dsoNameService.getName(b) === 'ORIGINAL');
+          if (originalBundle.length > 0) {
+            const bitstreamObservables = originalBundle.map(b => {
               return b.bitstreams.pipe(
                 getAllSucceededRemoteDataPayload(),
                 map((bitstreams: PaginatedList<Bitstream>) =>
                   bitstreams.page
-                )
+                ),
+                defaultIfEmpty([])
               );
             });
+            return combineLatest(bitstreamObservables).pipe(
+              map(arraysOfBitstreams => arraysOfBitstreams.reduce((acc, val) => acc.concat(val), []))
+            );
           }
           return [];
         })
       );
     }
-
-    return of(of([]));
+    return of([]);
   }
 }
