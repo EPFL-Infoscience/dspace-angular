96813207bbfca6d222f083ae384c9f4da5753f64
Julien Sicot <julien.sicot@epfl.ch>
2024-02-08 20:11:57 +0100

    EPFL - introduces new infoscience theme
    (cherry picked from commit 839ecf1dd645d6ca06e8b082a89642acbc762761)

---
+++ src/themes/infoscience/app/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { BreadcrumbsComponent as BaseComponent } from '../../../../app/breadcrumbs/breadcrumbs.component';
+
+/**
+ * Component representing the breadcrumbs of a page
+ */
+@Component({
+  selector: 'ds-breadcrumbs',
+  // templateUrl: './breadcrumbs.component.html',
+  templateUrl: './breadcrumbs.component.html',
+  // styleUrls: ['./breadcrumbs.component.scss']
+  styleUrls: ['./breadcrumbs.component.scss']
+})
+export class BreadcrumbsComponent extends BaseComponent {
+}
