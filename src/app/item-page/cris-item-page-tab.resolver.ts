af4b8fd916a161b9ec855827757abe2b2fb5a048
yevhenii.lohatskyi <lohatskyi.yevhenii@4science.com>
2024-02-13 13:45:31 +0200

    [CST-13578] fix cris-item-page-tab.resolver.ts to correctly handle case with only one leading tab

--- src/app/item-page/cris-item-page-tab.resolver.ts
+++ src/app/item-page/cris-item-page-tab.resolver.ts
@@ -54,7 +54,11 @@ export class CrisItemPageTabResolver implements Resolve<RemoteData<PaginatedList
                 const givenTab = urlSplit[1];
                 const itemPageRoute = getItemPageRoute(itemRD.payload);
                 const isValidTab = tabsRD.payload.page.some((tab) => !givenTab || `/${tab.shortname}` === givenTab);
-                const mainTab = tabsRD.payload.page.filter((tab) => !tab.leading)[0];
+                const mainTab = tabsRD.payload.page.length > 1
+                  ? tabsRD.payload.page.filter((tab) => !tab.leading)[0]
+                  : tabsRD.payload.page[0];
+                console.log('TabsRemoteData: ', tabsRD);
+                console.log('Main tab: ', mainTab);
                 if (!isValidTab) {
                   // If wrong tab is given redirect to 404 page
                   this.router.navigateByUrl(getPageNotFoundRoute(), { skipLocationChange: true, replaceUrl: false });
