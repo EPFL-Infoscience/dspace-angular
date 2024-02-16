c3f89a1d915eb1df9e2f40047c85fda8cd30f8b5
Alexander K <aliaksandr.kryvasheyeu@4science.com>
2024-02-16 13:50:09 +0100

    [CST-13598] hot fix

--- src/app/item-page/cris-item-page-tab.resolver.ts
+++ src/app/item-page/cris-item-page-tab.resolver.ts
@@ -53,14 +53,14 @@ export class CrisItemPageTabResolver implements Resolve<RemoteData<PaginatedList
                 const urlSplit = state.url.split(route.params.id);
                 const givenTab = urlSplit[1]?.split('/')[1];
                 const itemPageRoute = getItemPageRoute(itemRD.payload);
-                const isValidTab = tabsRD.payload.page.some((tab) => !givenTab || `/${tab.shortname}` === givenTab);
+                const isValidTab = tabsRD.payload.page.some((tab) => !givenTab || tab.shortname === givenTab);
                 const mainTab = tabsRD.payload.page.length > 1
                   ? tabsRD.payload.page.filter((tab) => !tab.leading)[0]
                   : tabsRD.payload.page[0];
                 if (!isValidTab) {
                   // If wrong tab is given redirect to 404 page
                   this.router.navigateByUrl(getPageNotFoundRoute(), { skipLocationChange: true, replaceUrl: false });
-                } else if (givenTab === `/${mainTab.shortname}`) {
+                } else if (givenTab === mainTab.shortname) {
                   // If first tab is given redirect to root item page
                   this.hardRedirectService.redirect(itemPageRoute, 302);
                 }
