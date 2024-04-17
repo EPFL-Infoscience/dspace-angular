d0a23e772e8dfda5caf74e2f7cd91b07409a5d73
aliaksei.bykau <aliaksei.bykau@4science.com>
2024-04-17 13:33:14 +0200

    [CST-14483] updated test in uploader.component.spec.ts

--- src/app/shared/upload/uploader/uploader.component.spec.ts
+++ src/app/shared/upload/uploader/uploader.component.spec.ts
@@ -1,6 +1,6 @@
 // Load the implementations that should be tested
 import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync, } from '@angular/core/testing';
+import { ComponentFixture, inject,  TestBed, waitForAsync, } from '@angular/core/testing';
 
 import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 
@@ -63,10 +63,10 @@ describe('UploaderComponent test', () => {
     testComp = testFixture.componentInstance;
   });
 
-  // it('should create Uploader Component', inject([UploaderComponent], (app: UploaderComponent) => {
-  //
-  //   expect(app).toBeDefined();
-  // }));
+  it('should create Uploader Component', inject([UploaderComponent], (app: UploaderComponent) => {
+
+    expect(app).toBeDefined();
+  }));
 
 });
 
@@ -84,7 +84,7 @@ class TestComponent {
   });
 
   /* eslint-disable no-empty,@typescript-eslint/no-empty-function */
-  public onBeforeUpload = () => {
+  public onAfterUpload = () => {
   };
 
   onCompleteItem(event) {
