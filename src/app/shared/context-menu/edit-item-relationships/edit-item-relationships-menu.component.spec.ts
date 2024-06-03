b9bf2afb666a62c7fc3311c721869bba5303161b
aliaksei.bykau <aliaksei.bykau@4science.com>
2024-06-03 08:43:05 +0200

    [CST-15028] checkstyle fixes

--- src/app/shared/context-menu/edit-item-relationships/edit-item-relationships-menu.component.spec.ts
+++ src/app/shared/context-menu/edit-item-relationships/edit-item-relationships-menu.component.spec.ts
@@ -20,8 +20,8 @@ import { CrisLayoutTab } from '../../../core/layout/models/tab.model';
 import { TAB } from '../../../core/layout/models/tab.resource-type';
 import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
 import { of as observableOf } from 'rxjs';
-import {APP_CONFIG} from "../../../../config/app-config.interface";
-import {environment} from "../../../../environments/environment";
+import {APP_CONFIG} from '../../../../config/app-config.interface';
+import {environment} from '../../../../environments/environment';
 
 describe('EditItemRelationshipsMenuComponent', () => {
   let component: EditItemRelationshipsMenuComponent;
