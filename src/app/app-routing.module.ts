aa11e8c32176f0139b4b4c503d1bffee8505b912
Andrea Barbasso <andrea.barbasso@4science.com>
2024-06-04 20:03:24 +0200

    [CST-21533] add options to disable the standard login
    # Conflicts:
    #	src/app/app-routing.module.ts
    #	src/app/shared/log-in/log-in.component.ts
    
    (cherry picked from commit 3945ea312b20c0a0936342fa07caabbfd75efb92)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,6 +45,7 @@ import { ThemedPageErrorComponent } from './page-error/themed-page-error.compone
 import { ForgotPasswordCheckGuard } from './core/rest-property/forgot-password-check-guard.guard';
 import { SUGGESTION_MODULE_PATH } from './suggestions-page/suggestions-page-routing-paths';
 import { RedirectService } from './redirect/redirect.service';
+import { environment } from '../environments/environment';
 import {
   CollectionAdministratorGuard
 } from './core/data/feature-authorization/feature-authorization-guard/collection-administrator.guard';
@@ -188,9 +189,19 @@ import {
               .then((m) => m.AdminDeduplicationPageModule),
           },
           {
+            path: 'standard-login',
+            loadChildren: () => import('./login-page/login-page.module').then((m) => m.LoginPageModule),
+            data: {
+              isBackDoor: true,
+            },
+          },
+          {
             path: 'login',
-            loadChildren: () => import('./login-page/login-page.module')
-              .then((m) => m.LoginPageModule)
+            loadChildren: () => import('./login-page/login-page.module').then((m) => m.LoginPageModule),
+            data: {
+              isBackDoor: false,
+            },
+            canMatch: [() => !environment.auth.disableStandardLogin],
           },
           {
             path: 'external-login/:token',
